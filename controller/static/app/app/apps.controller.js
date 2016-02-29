4b9b1db87d3d3e45af92270c123a424e86e68bc3
unknown <kaiden.gui@gmail.com>
2016-02-29 10:57:24 +0800

    add app manager

---
+++ controller/static/app/app/apps.controller.js
@@ -0,0 +1,42 @@
+(function(){
+	'use strict';
+
+	angular
+		.module('shipyard.apps')
+		.controller('AppsController', AppsController);
+
+	AppsController.$inject = ['apps', 'roles', 'AppsService', '$state', '$timeout'];
+	function AppsController(apps, roles, AppsService, $state, $timeout) {
+            var vm = this;
+            vm.apps = apps;
+            vm.refresh = refresh;
+            vm.selectedApp = null;
+            vm.removeApp = removeApp;
+            vm.showRemoveAppDialog = showRemoveAppDialog;
+
+            function showRemoveAppDialog(app) {
+                vm.selectedApp = app;
+                $('#remove-modal').modal('show');
+            }
+
+            function refresh() {
+                AppsService.list()
+                    .then(function(data) {
+                        vm.apps = data;
+                    }, function(data) {
+                        vm.error = data;
+                    });
+                vm.error = "";
+            }
+
+            function removeApp() {
+                AppsService.removeApp(vm.selectedApp)
+                    .then(function(data) {
+                        vm.refresh();
+                    }, function(data) {
+                        vm.error = data;
+                    });
+            }
+
+	}
+})();
